7acc4456b8ef79968b6ade9112fa2932b8f76421
alqaholic007 <soham.parekh1998@gmail.com>
2020-06-27 07:30:10 +0530

    feat: setup chat router

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   AccountRouter: require('./auth'),
   ApiRouter: require('./api/v1'),
+  ChatRouter: require('./chat'),
   IndexRouter: require('./main'),
   PostRouter: require('./post'),
   UserRouter: require('./user')
